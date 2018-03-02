521fecc2a387c7a5c341b8abe846901b1eb71502
Leon de França Nascimento <l.nasc@live.com>
2018-03-02 13:31:52 +0000

    [DEV] Suporte básico a idiomas

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
+session_start();
 
-include_once("VisuHTML.html");
-
+if(isset($_GET)){
+    
+    if(!$_GET['lang']){
+        $_SESSION['lang']='pt';
+    }
+    
+    
+    $_SESSION['lang'] = $_GET['lang'];
+}
+else
+    include_once("VisuHTML_".$_SESSION['lang'].html)
+    
  ?>
