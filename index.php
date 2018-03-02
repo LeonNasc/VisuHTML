478285c32fb87a4e23e9d9bd6a2c8ca0fe72d13c
Leon de França Nascimento <l.nasc@live.com>
2018-03-02 13:43:47 +0000

    [Bugfix] Php do index (2)

--- index.php
+++ index.php
@@ -2,13 +2,11 @@
 session_start();
 
 if(isset($_GET)){
-    
-    
     $_SESSION['lang'] = $_GET['lang'];
 }
 if(!$_GET['lang']){
         $_SESSION['lang']='pt';
 }
-
+var_dump($_SESSION['lang']);
 include_once("VisuHTML_".$_SESSION['lang'].html)    
  ?>
