00c65b8c9dfe09e1c4e6d9c8b3c2af9482a27860
Leon Nascimento <l.nasc@live.com>
2018-03-24 22:44:01 -0300

    [DEV] Adicionado placeholders

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 session_start();
 
-if(isset($_GET)){
-    $_SESSION['lang'] = $_GET['lang'];
-}
-if(!$_GET['lang']){
-        $_SESSION['lang']='pt';
-}
-
-include_once("VisuHTML_".$_SESSION['lang']. ".html");
+include_once("VisuHTML_pt.html");
  ?>
--- scripts.js
+++ scripts.js
@@ -27,3 +27,33 @@ function writeHTML(iframe,content){
   iframe.write(content);
   iframe.close();
 }
+
+function collapse_toggle(target_id){
+
+  let target = document.querySelector("#" + target_id);
+
+  if(target.classList.contains('show')){
+    target.classList.remove('show');
+  }
+  else{
+    target.classList.add('show');
+  }
+}
+
+function insert_placeholder(){
+
+  let form = document.querySelector('#placeholder_controls');
+  let info = form.querySelectorAll('input');
+
+  let height = info[0].value;
+  let width = info[1].value;
+
+  let html_content = "\n<div  style='background:radial-gradient(#dddddd,#888888);height:"+height+";width:"+width+";color:white;vertical-align:middle; text-align:center;font-family:sans-serif'></div>";
+
+  let target = document.querySelector('#HTML_area > textarea');
+  target.value += html_content;
+
+  //Necessário para aparecer a informação no iframe
+  target.dispatchEvent(new KeyboardEvent('keyup'));
+
+}
